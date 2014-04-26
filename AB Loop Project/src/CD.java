0af3cbed59a16fc66005144aa9299fc01e4b6852
Hamid <islam.md1971@gmail.com>
2014-04-25 20:41:16 -0400

    add project

---
+++ AB Loop Project/src/CD.java
@@ -0,0 +1,29 @@
+
+public class CD 
+{
+	void moga()
+	{
+		for(int x=4; x<10; x++)
+		{
+			System.out.println(x+"x");
+			
+		}
+		for (int y=5; y<20; y++)
+		{
+			System.out.println(y+"y");
+		}
+	}
+	void moga2()
+	{
+		for(int a=5; a<15; a++)
+		{
+			System.out.println("Helo");
+			if(a==5)
+			{
+				break;
+			}
+		}
+	}
+	
+}  
+	
---
+++ AB Loop Project/src/MP.java
@@ -0,0 +1,13 @@
+
+public class MP
+{
+
+	public static void main(String[] args)
+	{
+
+		CD x= new CD();
+		x.moga();
+		x.moga2();
+	}
+
+}
